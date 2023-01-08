722ddc4662456853eddb7a1952ae9197221c5b44
Anthony Brogni <anthonybrog@gmail.com>
2023-01-08 11:58:31 -0600

    fix

--- src/main/java/frc/robot/commands/autonomous/AutonomousMode_2.java
+++ src/main/java/frc/robot/commands/autonomous/AutonomousMode_2.java
@@ -1,6 +1,6 @@
 package frc.robot.commands.autonomous;
 
-import frc.robot.commands.DriveForTime;
+import frc.robot.commands.DriveForTimeCommand;
 import frc.robot.commands.GyroTurnToAngle;
 import frc.robot.commands.autonomous.helperCommands.Wait;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
