3b721291e24723002dbb6f686d1ecae0e43e5077
Anthony Brogni <anthonybrog@gmail.com>
2023-01-08 10:21:17 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -79,6 +79,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("Extender Position", m_extenderSubsystem.currentSetpoint);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
